62d1feb1adfe8038fd343d816583c138119f5a80
CoderBdk <cobdgithub07@gmail.com>
2024-09-02 15:08:05 +0600

    Update README.md

--- app/src/main/java/com/coderbdk/tictactoe/ui/home/HomeScreen.kt
+++ app/src/main/java/com/coderbdk/tictactoe/ui/home/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.coderbdk.tictactoe.ui.home
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,9 +11,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme.colorScheme
@@ -20,8 +24,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -50,8 +57,8 @@ fun HomeScreen(navController: NavController) {
             .background(
                 brush = Brush.linearGradient(
                     listOf(
-                        colorScheme.primaryContainer,
                         colorScheme.surfaceDim,
+                        colorScheme.primaryContainer,
                         colorScheme.primaryContainer
                     )
                 )
@@ -84,6 +91,29 @@ fun HomeScreen(navController: NavController) {
 
         Column(
             modifier = Modifier
+                .wrapContentHeight()
+                .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+                .shadow(
+                    elevation = 4.dp,
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .background(
+                    color = colorScheme.surface,
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(start = 4.dp, end = 4.dp)
+                .padding(top = 2.dp)
+                .background(
+                   brush = Brush.linearGradient(
+                       listOf(
+                           colorScheme.primaryContainer,
+                           colorScheme.surfaceVariant,
+                           colorScheme.surfaceDim
+                       )
+                   ),
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(bottom = 8.dp)
                 .constrainAs(menu) {
                     start.linkTo(parent.start)
                     top.linkTo(share.bottom)
@@ -96,7 +126,7 @@ fun HomeScreen(navController: NavController) {
         ) {
             Text(
                 modifier = Modifier
-                    .padding(bottom = 40.dp),
+                    .padding(bottom = 16.dp),
                 text = "Tic \nTac Toe",
                 fontSize = 64.sp,
                 lineHeight = 64.sp,
@@ -106,7 +136,7 @@ fun HomeScreen(navController: NavController) {
                 style = TextStyle(
                     color = colorScheme.surface,
                     shadow = Shadow(
-                        color = colorScheme.primary,
+                        color = colorScheme.outline,
                         blurRadius = 8f
                     )
                 )
@@ -114,8 +144,17 @@ fun HomeScreen(navController: NavController) {
             ElevatedButton(
                 modifier = Modifier
                     .height(64.dp)
-                    .padding(start = 16.dp, end = 16.dp, bottom = 8.dp),
+                    .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
+                    .border(
+                        width = 1.dp,
+                        color = colorScheme.surface,
+                        shape = RoundedCornerShape(8.dp)
+                    ),
                 shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = colorScheme.surfaceVariant,
+                    contentColor = colorScheme.onSurfaceVariant
+                ),
                 onClick = { navigate(navController, Screen.OfflineTwoPlayer) }) {
                 Row(
                     Modifier.fillMaxWidth(),
@@ -132,9 +171,8 @@ fun HomeScreen(navController: NavController) {
                             .padding(4.dp)
                             .fillMaxWidth(),
                         text = "Offline Two Player".uppercase(),
-                        fontSize = 24.sp,
+                        fontSize = 22.sp,
                         textAlign = TextAlign.Justify,
-                        fontWeight = FontWeight.Bold,
                     )
                 }
 
@@ -142,8 +180,17 @@ fun HomeScreen(navController: NavController) {
             ElevatedButton(
                 modifier = Modifier
                     .height(64.dp)
-                    .padding(start = 16.dp, end = 16.dp, bottom = 8.dp),
+                    .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
+                    .border(
+                        width = 1.dp,
+                        color = colorScheme.surface,
+                        shape = RoundedCornerShape(8.dp)
+                    ),
                 shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = colorScheme.surfaceVariant,
+                    contentColor = colorScheme.onSurfaceVariant
+                ),
                 onClick = { navigate(navController, Screen.OfflineVsComputer) }) {
                 Row(
                     Modifier.fillMaxWidth(),
@@ -159,7 +206,7 @@ fun HomeScreen(navController: NavController) {
                             .fillMaxWidth()
                             .padding(4.dp),
                         text = "Player Vs Computer".uppercase(),
-                        fontSize = 24.sp,
+                        fontSize = 22.sp,
                         textAlign = TextAlign.Justify
                     )
                 }
@@ -169,8 +216,17 @@ fun HomeScreen(navController: NavController) {
             ElevatedButton(
                 modifier = Modifier
                     .height(64.dp)
-                    .padding(start = 16.dp, end = 16.dp, bottom = 8.dp),
+                    .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
+                    .border(
+                        width = 1.dp,
+                        color = colorScheme.surface,
+                        shape = RoundedCornerShape(8.dp)
+                    ),
                 shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = colorScheme.surfaceVariant,
+                    contentColor = colorScheme.onSurfaceVariant
+                ),
                 onClick = { navigate(navController, Screen.OnlineTwoPlayer) }) {
                 Row(
                     Modifier.fillMaxWidth(),
@@ -186,7 +242,7 @@ fun HomeScreen(navController: NavController) {
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(4.dp),
-                        text = "Online Two Player".uppercase(), fontSize = 24.sp,
+                        text = "Online Two Player".uppercase(), fontSize = 22.sp,
                         textAlign = TextAlign.Justify
                     )
                 }
